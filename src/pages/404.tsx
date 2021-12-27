7938ee20c1e1cbb0d48fbc57061d5d6818114193
Jacob Patel <jseanpatel@gmail.com>
2021-12-27 12:51:34 -0800

    fix: resolved build errors

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,15 +1,10 @@
 import * as React from 'react';
 
-import Seo from '@/components/Seo';
-
 import Hero from '../components/Hero';
 
 export default function ContactUs() {
   return (
     <>
-      {/* <Seo templateTitle='Home' /> */}
-      <Seo />
-
       <main>
         <section className="">
           <Hero title="404" description="Page not found" />
