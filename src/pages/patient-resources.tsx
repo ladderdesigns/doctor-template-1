76b97dcfc5fed7a28a4d814b5794e9f865c94c5b
Jacob Patel <jseanpatel@gmail.com>
2022-03-27 15:01:05 -0700

    fix: adjusted patient resource styling

--- src/pages/patient-resources.tsx
+++ src/pages/patient-resources.tsx
@@ -8,7 +8,11 @@ import Background from '../components/Background';
 import Hero from '../components/Hero';
 
 export default function PatientResources() {
-  const items = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  const items = [
+    { id: 0, name: 'new-patient-packet.pdf' },
+    { id: 1, name: 'catherization-instructions.pdf' },
+    { id: 2, name: 'nuclear-stress-test.pdf' },
+  ];
 
   return (
     <>
@@ -29,21 +33,21 @@ export default function PatientResources() {
                     Download forms below:
                   </h2>
                   <dl className="divide-gray-200 divide-y mt-6 space-y-6">
-                    {items.map((service) => (
-                      <Disclosure as="div" key={service.id} className="pt-6">
+                    {items.map((file) => (
+                      <Disclosure as="div" key={file.id} className="pt-6">
                         {({ open }) => (
                           <>
                             <dt className="text-lg">
                               <Disclosure.Button className="flex items-start justify-between text-gray-400 text-left w-full">
                                 <span className="font-medium text-gray-900">
-                                  {service.id}
+                                  {file.name}
                                 </span>
                                 <span className="flex h-7 items-center ml-6">
                                   <button
                                     type="button"
                                     className="bg-blue-600 border border-transparent focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 font-medium hover:bg-blue-700 inline-flex items-center leading-4 px-3 py-2 rounded-md shadow-sm text-sm text-white"
                                   >
-                                    Button text
+                                    Download
                                     <DownloadIcon
                                       className="-mr-0.5 h-4 ml-2 w-4"
                                       aria-hidden="true"
@@ -52,11 +56,6 @@ export default function PatientResources() {
                                 </span>
                               </Disclosure.Button>
                             </dt>
-                            <Disclosure.Panel as="dd" className="mt-2 pr-12">
-                              <p className="text-base text-gray-500">
-                                {service.id}
-                              </p>
-                            </Disclosure.Panel>
                           </>
                         )}
                       </Disclosure>
