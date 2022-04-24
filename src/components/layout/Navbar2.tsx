4d8fcb638f0884b65814f4a16f744a287f0665ea
Jacob Patel <jseanpatel@gmail.com>
2022-04-23 22:53:30 -0700

    Revert "chore: added honeypot field to contactform"
    This reverts commit 6a3d2f697627a3a27ce9472c319a2c628b087a4c.

--- src/components/layout/Navbar2.tsx
+++ src/components/layout/Navbar2.tsx
@@ -50,6 +50,7 @@ export default function Navbar2() {
                       Our Services
                     </a>
                   </Link>
+                  {/* TODO: color should remain the same when you click on the Navbar links */}
                   <Link href="/patient-forms">
                     <a
                       href="/patient-forms"
